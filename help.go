ea5fefcb5ec0047bdb81e903dc3a361a42c9faa4
Khash Sajadi <khash@cloud66.com>
2014-04-07 21:19:18 +0200

    updated the list of environment variables used in the help

--- help.go
+++ help.go
@@ -31,10 +31,24 @@ CXDEBUG
   response. This will most likely include your secret API key in
   the Authorization header field, so be careful with the output.
 
+CXENVIRONMENT
+	When this is set, cx looks for an environment specific OAuth token file.
+	Normally the authentication file is called cx.json as is located under ~/.cloud66
+	directory. Using this environment variable, cx will look for cx_<environment>.json
+	file instead.
+
+CX_APP_ID, CX_APP_SECRET
+	Normally cx uses production environment app ID and secret. When this is set, the normal app ID
+	and secrects are overwritten. This is used for debugging and development purposes.
+
 CXSTACK
 
 	If set, it overrides the stack sent as a parameter to the commands.
 	This can be the name of the stack or its UID.
+
+CXTOKEN
+	Anything set to this will be passed as X-CxToken HTTP header to the server.
+	Used for development purposes.
 `,
 }
 
