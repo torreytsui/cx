64f0a7511783f0c56e48ff83abaa4958e7dd5efe
Khash Sajadi <khash@cloud66.com>
2014-07-10 18:27:12 -0500

    auto complete for install scripts

--- extra/install-osx.sh
+++ extra/install-osx.sh
@@ -22,6 +22,17 @@
   wget -qO- $C66_CLIENT_URL | tar xz --strip=1
   fi
 
+  if [[ -z "$(which wget)" ]]; then
+  curl -so /usr/local/cloud66/completion.bash.inc https://raw.githubusercontent.com/cloud66/cx/master/extra/completion.bash.inc
+  curl -so /usr/local/cloud66/add-completion.sh https://raw.githubusercontent.com/cloud66/cx/master/extra/add-completion.sh
+  else
+  wget -q https://raw.githubusercontent.com/cloud66/cx/master/extra/completion.bash.inc -P /usr/local/cloud66 > /dev/null
+  wget -q https://raw.githubusercontent.com/cloud66/cx/master/extra/add-completion.sh -P /usr/local/cloud66 > /dev/null
+  fi
+
+  chmod +x /usr/local/cloud66/add-completion.sh
+  /usr/local/cloud66/add-completion.sh
+
 SCRIPT
 
   # remind the user to add to $PATH
--- extra/install-ubuntu.sh
+++ extra/install-ubuntu.sh
@@ -11,16 +11,27 @@
 		# run inside sudo
 		sudo sh <<SCRIPT
 
-		# download and extract the client tarball
-		rm -rf /usr/local/cloud66
-		mkdir -p /usr/local/cloud66
-		cd /usr/local/cloud66
+	# download and extract the client tarball
+	rm -rf /usr/local/cloud66
+	mkdir -p /usr/local/cloud66
+	cd /usr/local/cloud66
 
-		if [[ -z "$(which wget)" ]]; then
-		curl -s $C66_CLIENT_URL | tar xz --strip=1
-		else
-		wget -qO- $C66_CLIENT_URL | tar xz --strip=1
-		fi
+	if [[ -z "$(which wget)" ]]; then
+	curl -s $C66_CLIENT_URL | tar xz --strip=1
+	else
+	wget -qO- $C66_CLIENT_URL | tar xz --strip=1
+	fi
+
+	if [[ -z "$(which wget)" ]]; then
+	curl -so /usr/local/cloud66/completion.bash.inc https://raw.githubusercontent.com/cloud66/cx/master/extra/completion.bash.inc
+	curl -so /usr/local/cloud66/add-completion.sh https://raw.githubusercontent.com/cloud66/cx/master/extra/add-completion.sh
+	else
+	wget -q https://raw.githubusercontent.com/cloud66/cx/master/extra/completion.bash.inc -P /usr/local/cloud66 > /dev/null
+	wget -q https://raw.githubusercontent.com/cloud66/cx/master/extra/add-completion.sh -P /usr/local/cloud66 > /dev/null
+	fi
+
+	chmod +x /usr/local/cloud66/add-completion.sh
+	/usr/local/cloud66/add-completion.sh
 
 SCRIPT
 
