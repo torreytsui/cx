26e93916d7c23a3352814c0dac317013a4b8ffb8
Daniel van Gils <daniel@cloud66.com>
2016-04-12 23:26:50 +0200

    mock http calls

--- update_test.go
+++ update_test.go
@@ -3,13 +3,20 @@ package main
 import (
 	. "github.com/onsi/ginkgo"
     . "github.com/onsi/gomega"
+    "gopkg.in/h2non/gock.v0"
 )
 
 var _ = Describe("Update", func() {
 	Context("a new version", func() {
 		It("should retreive the newest version", func() {
-			_, err := findLatestVersion()
-			//Expect(string(response.Version)).To(Equal("0.1.39"))
+			defer gock.Off()
+			gock.New("http://downloads.cloud66.com/cx").
+				Get("/cx_latest.json").
+    			Reply(200).
+    			BodyString("{\"latest\":\"0.1.40\"}")
+
+  			response, err := findLatestVersion()
+			Expect(string(response.Version)).To(Equal("0.1.40"))
 			Expect(err).NotTo(HaveOccurred())
 		})
 	})
