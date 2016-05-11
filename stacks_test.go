0fede244a65345d3243346765ca84f8f657fb208
Khash Sajadi <khash@cloud66.com>
2016-05-11 11:21:03 -0700

    fixing tests causing issue when run on a non UTC timezone

--- stacks_test.go
+++ stacks_test.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"flag"
+
+	"github.com/cloud66/cli"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/cloud66/cli"
-	"flag"
 )
 
 var _ = Describe("Stacks list command", func() {
 	Context("with a customer having three stacks in production", func() {
-		
+
 		var flagSet *flag.FlagSet
 
 		BeforeEach(func() {
@@ -26,41 +27,41 @@ var _ = Describe("Stacks list command", func() {
 			// run context with the CLI
 			context := cli.NewContext(nil, flagSet, nil)
 			runStacks(context)
-			
+
 			// read stdout
 			output := StopCaptureStdout()
 
 			// check the actual output
-			Expect(output[0]).To(Equal("Awesome App1  production  Deployed successfully  Aug 14  2014"))
-			Expect(len(output)-1).To(Equal(3))
+			Expect(output[0]).To(HavePrefix("Awesome App1  production  Deployed successfully"))
+			Expect(len(output) - 1).To(Equal(3))
 		})
 
 		It("will show all three stacks running production when no flag is specified", func() {
 			// run context with the CLI
 			context := cli.NewContext(nil, flagSet, nil)
 			runStacks(context)
-			
+
 			// read stdout
 			output := StopCaptureStdout()
 
 			// check the actual output
-			Expect(output[0]).To(Equal("Awesome App1  production  Deployed successfully  Aug 14  2014"))
-			Expect(len(output)-1).To(Equal(3))
+			Expect(output[0]).To(HavePrefix("Awesome App1  production  Deployed successfully"))
+			Expect(len(output) - 1).To(Equal(3))
 		})
 
 		It("will show all zero stacks running in development", func() {
 			//  set the enviroment flag to development
 			flagSet.String("environment", "development", "")
-		
+
 			// run context with the CLI
 			context := cli.NewContext(nil, flagSet, nil)
 			runStacks(context)
-			
+
 			// read stdout
 			output := StopCaptureStdout()
 
 			// check the actual output
-			Expect(len(output)-1).To(Equal(0))
+			Expect(len(output) - 1).To(Equal(0))
 		})
 
 	})
