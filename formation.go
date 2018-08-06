8ecf800759eaa90b31b9af888d8cb6bc5045c24c
Khash Sajadi <khash@cloud66.com>
2018-08-06 14:01:21 +0100

    formation bundle commands

--- formation.go
+++ formation.go
@@ -10,7 +10,6 @@ import (
 	"path/filepath"
 	"sort"
 	"strings"
-
 	"text/tabwriter"
 
 	"github.com/cloud66-oss/cloud66"
@@ -30,7 +29,7 @@ func buildFormations() cli.Command {
 	base.Subcommands = []cli.Command{
 		cli.Command{
 			Name:   "list",
-			Action: runFormations,
+			Action: runListFormations,
 			Usage:  "lists all the formations of a stack.",
 			Description: `List all the formations of a stack.
 The information contains the name and UUID
@@ -41,12 +40,35 @@ $ cx formations list -s mystack foo bar // only show formations foo and bar
 `,
 		},
 		cli.Command{
+			Name:   "create",
+			Action: runCreateFormation,
+			Usage:  "Create a new formation",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "name",
+					Usage: "Formation name",
+				},
+				cli.StringFlag{
+					Name:  "template-repo",
+					Usage: "Base Template repository URL",
+				},
+				cli.StringFlag{
+					Name:  "template-branch",
+					Usage: "Base Template repository branch",
+				},
+				cli.StringFlag{
+					Name:  "tags",
+					Usage: "Formation tags",
+				},
+			},
+		},
+		cli.Command{
 			Name:  "bundle",
 			Usage: "formation bundle commands",
 			Subcommands: []cli.Command{
 				cli.Command{
 					Name:   "download",
-					Action: runDownloadBundle,
+					Action: runBundleDownload,
 					Usage:  "Specify the formation to use",
 					Flags: []cli.Flag{
 						cli.StringFlag{
@@ -67,6 +89,29 @@ $ cx formations list -s mystack foo bar // only show formations foo and bar
 						},
 					},
 				},
+				cli.Command{
+					Name:   "upload",
+					Usage:  "Upload a formation bundle to a new formation",
+					Action: runBundleUpload,
+					Flags: []cli.Flag{
+						cli.StringFlag{
+							Name:  "formation",
+							Usage: "Name for the new formation",
+						},
+						cli.StringFlag{
+							Name:  "stack,s",
+							Usage: "full or partial stack name. This can be omitted if the current directory is a stack directory",
+						},
+						cli.StringFlag{
+							Name:  "file",
+							Usage: "filename for the bundle file",
+						},
+						cli.StringFlag{
+							Name:  "message",
+							Usage: "Commit message",
+						},
+					},
+				},
 			},
 		},
 		cli.Command{
@@ -76,7 +121,7 @@ $ cx formations list -s mystack foo bar // only show formations foo and bar
 				cli.Command{
 					Name:   "list",
 					Usage:  "List all formation stencils",
-					Action: runStencilsList,
+					Action: runListStencils,
 					Flags: []cli.Flag{
 						cli.StringFlag{
 							Name:  "formation",
@@ -116,6 +161,45 @@ $ cx formations stencils list --formation bar
 						},
 					},
 				},
+				cli.Command{
+					Name:   "add",
+					Usage:  "Add a stencil to the formation",
+					Action: runAddStencil,
+					Flags: []cli.Flag{
+						cli.StringFlag{
+							Name:  "formation",
+							Usage: "Specify the formaiton to use",
+						},
+						cli.StringFlag{
+							Name:  "stack,s",
+							Usage: "Full or partial stack name. This can be omitted if the current directory is a stack directory",
+						},
+						cli.StringFlag{
+							Name:  "stencil",
+							Usage: "Stencil file",
+						},
+						cli.StringFlag{
+							Name:  "service",
+							Usage: "Service context of the stencil, if applicable",
+						},
+						cli.StringFlag{
+							Name:  "template",
+							Usage: "Template filename",
+						},
+						cli.IntFlag{
+							Name:  "sequence",
+							Usage: "Stencil sequence",
+						},
+						cli.StringFlag{
+							Name:  "message",
+							Usage: "Commit message",
+						},
+						cli.StringFlag{
+							Name:  "tags",
+							Usage: "Comma separated tags",
+						},
+					},
+				},
 			},
 		},
 	}
@@ -123,7 +207,8 @@ $ cx formations stencils list --formation bar
 	return base
 }
 
-func runFormations(c *cli.Context) {
+/* Formations */
+func runListFormations(c *cli.Context) {
 	stack := mustStack(c)
 	w := tabwriter.NewWriter(os.Stdout, 1, 2, 2, ' ', 0)
 	defer w.Flush()
@@ -154,78 +239,27 @@ func runFormations(c *cli.Context) {
 	}
 }
 
-func runStencilsList(c *cli.Context) {
+func runCreateFormation(c *cli.Context) {
 	stack := mustStack(c)
-	w := tabwriter.NewWriter(os.Stdout, 1, 2, 2, ' ', 0)
-	defer w.Flush()
 
-	formationName := c.String("formation")
-	if formationName == "" {
-		printFatal("No formation provided. Please use --formation to specify a formation")
+	tags := []string{}
+	name := c.String("name")
+	templateRepo := c.String("template-repo")
+	templateBranch := c.String("template-branch")
+	tagList := c.String("tags")
+	if tagList != "" {
+		tags = strings.Split(tagList, ",")
 	}
 
-	var formations []cloud66.Formation
-	var err error
-	formations, err = client.Formations(stack.Uid, true)
-	must(err)
-
-	output := c.String("output")
-	if output == "" {
-		output = "standard"
+	_, err := client.CreateFormation(stack.Uid, name, templateRepo, templateBranch, tags)
+	if err != nil {
+		printFatal(err.Error())
 	}
 
-	for _, formation := range formations {
-		if formation.Name == formationName {
-			printStencils(w, formation, output)
-			return
-		}
-	}
-
-	printFatal("No formation named '%s' found", formationName)
+	fmt.Println("Formation created")
 }
 
-func runShowStencil(c *cli.Context) {
-	stack := mustStack(c)
-	w := tabwriter.NewWriter(os.Stdout, 1, 2, 2, ' ', 0)
-	defer w.Flush()
-
-	formationName := c.String("formation")
-	if formationName == "" {
-		printFatal("No formation provided. Please use --formation to specify a formation")
-	}
-
-	stencilName := c.String("stencil")
-	if stencilName == "" {
-		printFatal("No stencil name provided. Please use --stencil to specify a formation")
-	}
-
-	var formations []cloud66.Formation
-	var err error
-	formations, err = client.Formations(stack.Uid, true)
-	must(err)
-
-	foundStencil := false
-
-	for _, formation := range formations {
-		if formation.Name == formationName {
-			for _, stencil := range formation.Stencils {
-				if stencil.Filename == stencilName {
-					printStencil(stencil)
-					foundStencil = true
-				}
-			}
-
-			if !foundStencil {
-				printFatal("No stencil named '%s' found", stencilName)
-			}
-			return
-		}
-	}
-
-	printFatal("No formation named '%s' found", formationName)
-}
-
-func runDownloadBundle(c *cli.Context) {
+func runBundleDownload(c *cli.Context) {
 	stack := mustStack(c)
 
 	formationName := c.String("formation")
@@ -264,55 +298,108 @@ func runDownloadBundle(c *cli.Context) {
 	printFatal("No formation named '%s' found", formationName)
 }
 
-func printStencils(w io.Writer, formation cloud66.Formation, output string) {
-	stencils := formation.Stencils
-	sort.Sort(stencilBySequence(stencils))
+func runBundleUpload(c *cli.Context) {
+	stack := mustStack(c)
 
-	if output == "standard" {
-		listRec(w,
-			"UID",
-			"FILENAME",
-			"TAGS",
-			"CREATED AT",
-			"LAST UPDATED")
-	} else {
-		listRec(w,
-			"UID",
-			"FILENAME",
-			"SERVICE",
-			"TAGS",
-			"TEMPLATE",
-			"GITFILE",
-			"INLINE",
-			"CREATED AT",
-			"LAST UPDATED")
+	formationName := c.String("formation")
+	if formationName == "" {
+		printFatal("No formation provided. Please use --formation to specify a formation")
 	}
 
-	for _, a := range stencils {
-		listStencil(w, a, output)
+	bundleFile := c.String("file")
+	if bundleFile == "" {
+		bundleFile = formationName + ".formation"
 	}
+
+	tagList := c.String("tags")
+	tags := []string{}
+	if tagList == "" {
+		tags = strings.Split(tagList, ",")
+	}
+
+	// untar the bundle
+	bundleTopPath, err := ioutil.TempDir("", fmt.Sprintf("%s-formation-bundle-", formationName))
+	if err != nil {
+		printFatal(err.Error())
+	}
+
+	err = Untar(bundleFile, bundleTopPath)
+	if err != nil {
+		printFatal(err.Error())
+	}
+	bundlePath := filepath.Join(bundleTopPath, "bundle")
+	manifestFile := filepath.Join(bundlePath, "manifest.json")
+	message := c.String("message")
+	if message == "" {
+		printFatal("No message given. Use --message to provide a message for the commit")
+	}
+
+	// load the bundle manifest
+	bundle, err := os.Open(manifestFile)
+	if err != nil {
+		printFatal(err.Error())
+	}
+	defer bundle.Close()
+
+	buff, err := ioutil.ReadAll(bundle)
+	if err != nil {
+		printFatal(err.Error())
+	}
+
+	var fb *cloud66.FormationBundle
+	err = json.Unmarshal(buff, &fb)
+	if err != nil {
+		printFatal(err.Error())
+	}
+
+	// create the formation
+	fmt.Printf("Creating %s formation...\n", formationName)
+	formation, err := client.CreateFormation(stack.Uid, formationName, fb.BaseTemplate.Repo, fb.BaseTemplate.Branch, tags)
+	if err != nil {
+		printFatal(err.Error())
+	}
+	fmt.Println("Formation created")
+
+	// add stencils
+	fmt.Println("Adding stencils...")
+	stencils := make([]*cloud66.Stencil, len(fb.Stencils))
+	for idx, stencil := range fb.Stencils {
+		stencils[idx], err = stencil.AsStencil(bundlePath)
+		if err != nil {
+			printFatal(err.Error())
+		}
+	}
+	_, err = client.AddStencils(stack.Uid, formation.Uid, stencils, message)
+	if err != nil {
+		printFatal(err.Error())
+	}
+	fmt.Println("Stencils added")
+
+	// TODO: add stencil groups
+	// TODO: add policies
 }
 
 func bundleFormation(formation cloud66.Formation, bundleFile string) {
 	// build a temp folder structure
-	dir, err := ioutil.TempDir("", fmt.Sprintf("%s-formation-bundle-", formation.Name))
+	topDir, err := ioutil.TempDir("", fmt.Sprintf("%s-formation-bundle-", formation.Name))
 	if err != nil {
 		printFatal(err.Error())
 	}
+	dir := filepath.Join(topDir, "bundle")
 
 	//defer os.RemoveAll(dir)
 	stencilsDir := filepath.Join(dir, "stencils")
-	err = os.Mkdir(stencilsDir, os.ModePerm)
+	err = os.MkdirAll(stencilsDir, os.ModePerm)
 	if err != nil {
 		printFatal(err.Error())
 	}
 	stencilGroupsDir := filepath.Join(dir, "stencil-groups")
-	err = os.Mkdir(stencilGroupsDir, os.ModePerm)
+	err = os.MkdirAll(stencilGroupsDir, os.ModePerm)
 	if err != nil {
 		printFatal(err.Error())
 	}
 	policiesDir := filepath.Join(dir, "policies")
-	err = os.Mkdir(policiesDir, os.ModePerm)
+	err = os.MkdirAll(policiesDir, os.ModePerm)
 	if err != nil {
 		printFatal(err.Error())
 	}
@@ -384,11 +471,20 @@ func bundleFormation(formation cloud66.Formation, bundleFile string) {
 	fmt.Printf("Bundle is saved to %s\n", bundleFile)
 }
 
-func printStencil(stencil cloud66.Stencil) {
-	var buffer bytes.Buffer
+func listFormation(w io.Writer, a cloud66.Formation) {
+	ta := a.CreatedAt
 
-	buffer.WriteString(stencil.Body)
-	fmt.Print(buffer.String())
+	listRec(w,
+		a.Uid,
+		a.Name,
+		a.Tags,
+		len(a.Stencils),
+		len(a.StencilGroups),
+		len(a.Policies),
+		a.BaseTemplate,
+		prettyTime{ta},
+		prettyTime{a.UpdatedAt},
+	)
 }
 
 func printFormationList(w io.Writer, formations []cloud66.Formation) {
@@ -412,20 +508,191 @@ func printFormationList(w io.Writer, formations []cloud66.Formation) {
 	}
 }
 
-func listFormation(w io.Writer, a cloud66.Formation) {
-	ta := a.CreatedAt
+type formationByName []cloud66.Formation
 
-	listRec(w,
-		a.Uid,
-		a.Name,
-		a.Tags,
-		len(a.Stencils),
-		len(a.StencilGroups),
-		len(a.Policies),
-		a.BaseTemplate,
-		prettyTime{ta},
-		prettyTime{a.UpdatedAt},
-	)
+func (a formationByName) Len() int           { return len(a) }
+func (a formationByName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a formationByName) Less(i, j int) bool { return a[i].Name < a[j].Name }
+
+/* End Formations */
+
+/* Stencils */
+func runListStencils(c *cli.Context) {
+	stack := mustStack(c)
+	w := tabwriter.NewWriter(os.Stdout, 1, 2, 2, ' ', 0)
+	defer w.Flush()
+
+	formationName := c.String("formation")
+	if formationName == "" {
+		printFatal("No formation provided. Please use --formation to specify a formation")
+	}
+
+	var formations []cloud66.Formation
+	var err error
+	formations, err = client.Formations(stack.Uid, true)
+	must(err)
+
+	output := c.String("output")
+	if output == "" {
+		output = "standard"
+	}
+
+	for _, formation := range formations {
+		if formation.Name == formationName {
+			printStencils(w, formation, output)
+			return
+		}
+	}
+
+	printFatal("No formation named '%s' found", formationName)
+}
+
+func runShowStencil(c *cli.Context) {
+	stack := mustStack(c)
+
+	formationName := c.String("formation")
+	if formationName == "" {
+		printFatal("No formation provided. Please use --formation to specify a formation")
+	}
+
+	stencilName := c.String("stencil")
+	if stencilName == "" {
+		printFatal("No stencil name provided. Please use --stencil to specify a formation")
+	}
+
+	var formations []cloud66.Formation
+	var err error
+	formations, err = client.Formations(stack.Uid, true)
+	must(err)
+
+	foundStencil := false
+
+	for _, formation := range formations {
+		if formation.Name == formationName {
+			for _, stencil := range formation.Stencils {
+				if stencil.Filename == stencilName {
+					printStencil(stencil)
+					foundStencil = true
+				}
+			}
+
+			if !foundStencil {
+				printFatal("No stencil named '%s' found", stencilName)
+			}
+			return
+		}
+	}
+
+	printFatal("No formation named '%s' found", formationName)
+}
+
+func runAddStencil(c *cli.Context) {
+	stack := mustStack(c)
+
+	formationName := c.String("formation")
+	if formationName == "" {
+		printFatal("No formation provided. Please use --formation to specify a formation")
+	}
+
+	stencilFile := c.String("stencil")
+	if stencilFile == "" {
+		printFatal("No stencil filename provided. Please use --stencil to specify a stencil file")
+	}
+
+	tags := []string{}
+	contextID := c.String("service")
+	template := c.String("template")
+	sequence := c.Int("sequence")
+	message := c.String("message")
+	tagList := c.String("tags")
+	if tagList != "" {
+		tags = strings.Split(tagList, ",")
+	}
+
+	var formations []cloud66.Formation
+	var err error
+	formations, err = client.Formations(stack.Uid, true)
+	must(err)
+	var foundFormation cloud66.Formation
+
+	for _, formation := range formations {
+		if formation.Name == formationName {
+			for _, stencil := range formation.Stencils {
+				if stencil.Filename == stencilFile {
+					// there is a stencil with the same name. abort
+					printFatal("Another stencil with the same name is found. You can use the update command to update it")
+					return
+				}
+			}
+			foundFormation = formation
+		}
+	}
+
+	if err := addStencil(stack, &foundFormation, stencilFile, contextID, template, sequence, message, tags); err != nil {
+		printFatal(err.Error())
+	}
+
+	fmt.Println("Stencil was added to formation")
+}
+
+func printStencils(w io.Writer, formation cloud66.Formation, output string) {
+	stencils := formation.Stencils
+	sort.Sort(stencilBySequence(stencils))
+
+	if output == "standard" {
+		listRec(w,
+			"UID",
+			"FILENAME",
+			"TAGS",
+			"CREATED AT",
+			"LAST UPDATED")
+	} else {
+		listRec(w,
+			"UID",
+			"FILENAME",
+			"SERVICE",
+			"TAGS",
+			"TEMPLATE",
+			"GITFILE",
+			"INLINE",
+			"CREATED AT",
+			"LAST UPDATED")
+	}
+
+	for _, a := range stencils {
+		listStencil(w, a, output)
+	}
+}
+
+func printStencil(stencil cloud66.Stencil) {
+	var buffer bytes.Buffer
+
+	buffer.WriteString(stencil.Body)
+	fmt.Print(buffer.String())
+}
+
+func addStencil(stack *cloud66.Stack, formation *cloud66.Formation, stencilFile string, contextID string, templateFilename string, sequence int, message string, tags []string) error {
+	body, err := ioutil.ReadFile(stencilFile)
+	if err != nil {
+		return err
+	}
+
+	remoteFilename := filepath.Base(stencilFile)
+	stencil := &cloud66.Stencil{
+		Filename:         remoteFilename,
+		TemplateFilename: templateFilename,
+		ContextID:        contextID,
+		Tags:             tags,
+		Body:             string(body),
+		Sequence:         sequence,
+	}
+
+	_, err = client.AddStencils(stack.Uid, formation.Uid, []*cloud66.Stencil{stencil}, message)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func listStencil(w io.Writer, a cloud66.Stencil, output string) {
@@ -453,14 +720,10 @@ func listStencil(w io.Writer, a cloud66.Stencil, output string) {
 	}
 }
 
-type formationByName []cloud66.Formation
-
-func (a formationByName) Len() int           { return len(a) }
-func (a formationByName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a formationByName) Less(i, j int) bool { return a[i].Name < a[j].Name }
-
 type stencilBySequence []cloud66.Stencil
 
 func (a stencilBySequence) Len() int           { return len(a) }
 func (a stencilBySequence) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a stencilBySequence) Less(i, j int) bool { return a[i].Sequence < a[j].Sequence }
+
+/* End Stencils */
